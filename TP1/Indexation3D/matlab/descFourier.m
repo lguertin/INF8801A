6877296888bad3a730d1107128588b00adfa14a0
Younes Ijichi <younes.ijichi@polymtl.ca>
2019-09-12 14:19:34 -0400

    1st part desc fourrier

--- TP1/Indexation3D/matlab/descFourier.m
+++ TP1/Indexation3D/matlab/descFourier.m
@@ -19,6 +19,43 @@ classdef descFourier
             % Vous pouvez utiliser les fonctions matlab :
             % bwtraceboundary, interp1, etc..
             
+            center = [128, 128];
+            [length_x, length_y] = size(shape);
+
+            
+            center_found = 0;
+            for x = 1:length_x
+                for y = 1:length_y
+                    if shape(x, y) ~= 0
+                        center_found = 1;
+                        center = [x, y];
+                        break
+                    end
+                end
+                
+                if center_found == 1
+                    break
+                end
+            end
+            
+            contour = bwtraceboundary(shape, center, 'N');
+%             figure, plot(contour(:,2),contour(:,1),'g','LineWidth',2)
+%             keyboard
+            
+
+            [number_of_points, number_of_dimensions] = size(contour);
+            
+            Zk = 0;
+            
+            for j = 1:number_of_points
+                z = complex(contour(j, 1), contour(j, 2));
+                i = complex(0, 1);
+                
+                Zk += 
+                
+            end++
+           
+            
             % TODO Question 1 :
             dst.values = zeros(1,descFourier.descSize);
             
