675c02899727c4a00e27f6c385a556cdcd0658fe
Leandre Guertin <leandre.guertin@polymtl.ca>
2019-11-12 21:49:17 -0500

    Ajout de getSeam

--- TP5/SeamCarving/matlab/getSeam.m
+++ TP5/SeamCarving/matlab/getSeam.m
@@ -5,5 +5,31 @@ function [ seam ] = getSeam( costs )
     % TODO : Question 2
     h = size(costs,1);
     seam = ones(h,1);
+    
+    % Find the minimum index of the costs matrix for the start of the
+    % algorithm
+    idx = find(costs(h) == min(costs(h)));
+    seam(h) = idx;
+    
+    i = h - 1;
+    while i > 0
+        % Find the 3 upper neighbours values of idx
+        indexes = [Inf, Inf, Inf];
+        if idx > 1
+            indexes(1) = costs(i, idx - 1);
+        end
+        
+        indexes(2) = costs(i, idx);
+        
+        if idx < h
+            indexes(3) = costs(i, idx + 1);
+        end
+        
+        idx = idx - 1 + find(indexes == min(indexes));
+        
+        seam(i) = idx;
+        
+        i = i - 1;
+    end
 end
 
